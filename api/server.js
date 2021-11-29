7d1e87e9fd1b3e2a887d0c85f4b10bc198cc3c97
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-29 14:32:49 -0800

    Connected server and some initial set up

--- api/server.js
+++ api/server.js
@@ -1,3 +1,34 @@
-// BUILD YOUR SERVER HERE
+// Imports
+const express = require('express')
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// Instance of express app
+const server=express();
+
+// Global Middleware
+server.use(express.json())
+
+// Endpoints
+// Get 'hello'
+server.get('/api/hello',(req, res) =>{
+    res.json({message:'hello'})
+})
+
+// Post a user
+// Get all users
+server.get('/api/users', (req, res) =>{
+    Users.find()
+        .then(users =>{
+            // throw new Error('foo')
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The users information could not be retrieved" })
+        })
+})
+
+// Get user by id
+// Delete a user by id
+// Put user by id
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
